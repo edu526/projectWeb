beba12b84d6e999a6d14b0bb7ddb8ccb90fa588f
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2016-11-29 00:10:17 -0500

    "Anaflavia"

--- Alquiler/src/java/negocio/LineaObj.java
+++ Alquiler/src/java/negocio/LineaObj.java
@@ -1,6 +1,44 @@
 
 package negocio;
 
+import java.util.Date;
+
 public class LineaObj {
+    
+    private String numAlq, codRut, hora;
+    private Date dia;
+
+    public String getNumAlq() {
+        return numAlq;
+    }
+
+    public void setNumAlq(String numAlq) {
+        this.numAlq = numAlq;
+    }
+
+    public String getCodRut() {
+        return codRut;
+    }
+
+    public void setCodRut(String codRut) {
+        this.codRut = codRut;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public Date getDia() {
+        return dia;
+    }
+
+    public void setDia(Date dia) {
+        this.dia = dia;
+    }
+
     
 }
