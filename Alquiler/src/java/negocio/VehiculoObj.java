bc57b63a29bdc17078fdb9e2abc026116bce2a0f
AnaflaviaDiaz <anaflaviadiazmartel5a@gmail.com>
2016-11-29 00:05:10 -0500

    "Anaflavia"

--- Alquiler/src/java/negocio/VehiculoObj.java
+++ Alquiler/src/java/negocio/VehiculoObj.java
@@ -2,4 +2,95 @@ package negocio;
 
 public class VehiculoObj {
     
+    private String codVeh, NFlota, matricuVeh, modelVeh, motorVeh, serieVeh, anioVeh, estadVeh, tipoVeh, codEmp;
+    private double precVeh;
+
+    public String getCodVeh() {
+        return codVeh;
+    }
+
+    public void setCodVeh(String codVeh) {
+        this.codVeh = codVeh;
+    }
+
+    public String getNFlota() {
+        return NFlota;
+    }
+
+    public void setNFlota(String NFlota) {
+        this.NFlota = NFlota;
+    }
+
+    public String getMatricuVeh() {
+        return matricuVeh;
+    }
+
+    public void setMatricuVeh(String matricuVeh) {
+        this.matricuVeh = matricuVeh;
+    }
+
+    public String getModelVeh() {
+        return modelVeh;
+    }
+
+    public void setModelVeh(String modelVeh) {
+        this.modelVeh = modelVeh;
+    }
+
+    public String getMotorVeh() {
+        return motorVeh;
+    }
+
+    public void setMotorVeh(String motorVeh) {
+        this.motorVeh = motorVeh;
+    }
+
+    public String getSerieVeh() {
+        return serieVeh;
+    }
+
+    public void setSerieVeh(String serieVeh) {
+        this.serieVeh = serieVeh;
+    }
+
+    public String getAnioVeh() {
+        return anioVeh;
+    }
+
+    public void setAnioVeh(String anioVeh) {
+        this.anioVeh = anioVeh;
+    }
+
+    public String getEstadVeh() {
+        return estadVeh;
+    }
+
+    public void setEstadVeh(String estadVeh) {
+        this.estadVeh = estadVeh;
+    }
+
+    public String getTipoVeh() {
+        return tipoVeh;
+    }
+
+    public void setTipoVeh(String tipoVeh) {
+        this.tipoVeh = tipoVeh;
+    }
+
+    public String getCodEmp() {
+        return codEmp;
+    }
+
+    public void setCodEmp(String codEmp) {
+        this.codEmp = codEmp;
+    }
+
+    public double getPrecVeh() {
+        return precVeh;
+    }
+
+    public void setPrecVeh(double precVeh) {
+        this.precVeh = precVeh;
+    }
+    
 }
